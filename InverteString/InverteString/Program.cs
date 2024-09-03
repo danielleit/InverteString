c62431ca356488c926023ef860f71ae0c95cb075
danielleit <81180528+danielleit@users.noreply.github.com>
2024-09-02 23:12:04 -0300

    Add files via upload

---
+++ InverteString/InverteString/Program.cs
@@ -0,0 +1,11 @@
+﻿string palavraParaTeste = "paralelepipedo";
+
+char[] arrayDeChar = palavraParaTeste.ToCharArray();
+
+string textoInvertido = "";
+
+for (int i = arrayDeChar.Length - 1; i >= 0; i--) {
+    textoInvertido += arrayDeChar[i];
+}
+
+Console.WriteLine(textoInvertido);
